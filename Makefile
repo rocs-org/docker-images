b4556ab72b7b17132d55f9eea6a56067076049ec
Jakob J. Kolb <kolb@hardfork.io>
2022-09-14 14:10:37 +0200

    Add poetry to docker image and add linting.

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 watch:
-	poetry run pytest -f -ff -x
+	poetry run pytest -v -f -ff -x
 
 test:
 	poetry run pytest -v
+
+# lint with black and pylama
+lint:
+	poetry run black . --check
+	poetry run pylama .
--- conftest.py
+++ conftest.py
@@ -2,19 +2,25 @@ import os
 
 import docker
 import pytest
-from docker.models.containers import Container
 
 
 class TrackedContainer:
-
     def __init__(self, dockerfile_path, image_name):
         self.client = docker.from_env()
         self.image = self.client.images.build(path=dockerfile_path, tag=image_name)[0]
         self.container = self.client.containers.run(self.image, detach=True, tty=True)
         print(self.container.logs())
 
-    def run(self, command):
-        return self.container.exec_run(command)
+    def run(self, command, **kwargs):
+        return self.container.exec_run(command, **kwargs)
+
+    @property
+    def api(self):
+        return self.client.api
+
+    @property
+    def id(self):
+        return self.container.id
 
     @property
     def status(self):
@@ -25,7 +31,7 @@ class TrackedContainer:
 
 
 @pytest.fixture(scope="function")
-def container(request) -> Container:
+def container(request) -> TrackedContainer:
     dockerfile_path = os.path.join(os.path.dirname(__file__), request.param)
     print(dockerfile_path)
     container = TrackedContainer(dockerfile_path, request.param)
