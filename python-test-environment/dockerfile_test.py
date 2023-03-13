5c0e453f510595acda4adbd147d6d4dccc3cb447
Jakob J. Kolb <kolbj@rki.de>
2023-03-13 21:48:46 +0100

    Add Docker file and tests for python test environment image for usage in jenkins.

--- python-test-environment/dockerfile_test.py
+++ python-test-environment/dockerfile_test.py
@@ -2,25 +2,25 @@ import pytest
 import subprocess
 
 
-@pytest.mark.parametrize("container", ["datascience-notebook"], indirect=["container"])
+@pytest.mark.parametrize("container", ["python-test-environment"], indirect=["container"])
 def test_jupyter(container):
     assert container.status == "created"
-    assert container.run("jupyter --version").exit_code == 0
+    assert container.run("poetry --version").exit_code == 0
 
 
-@pytest.mark.parametrize("container", ["datascience-notebook"], indirect=["container"])
+@pytest.mark.parametrize("container", ["python-test-environment"], indirect=["container"])
 def test_jupyter_git(container):
     assert container.status == "created"
-    assert container.run("git --version").exit_code == 0
+    assert container.run("make --version").exit_code == 0
 
 
-@pytest.mark.parametrize("container", ["datascience-notebook"], indirect=["container"])
+@pytest.mark.parametrize("container", ["python-test-environment"], indirect=["container"])
 def test_container_has_poetry(container):
     assert container.status == "created"
     assert container.run("poetry --version").exit_code == 0
 
 
-@pytest.mark.parametrize("container", ["datascience-notebook"], indirect=["container"])
+@pytest.mark.parametrize("container", ["python-test-environment"], indirect=["container"])
 def test_can_use_poetry_in_container(container, tmp_path):
     d = tmp_path / "pyproject.toml"
     d.parent.rmdir()
@@ -29,7 +29,7 @@ def test_can_use_poetry_in_container(container, tmp_path):
     d.write_text(PYPROJECT_CONTENT)
     # create valid pyproject.toml in container
     assert container.status == "created"
-    subprocess.run(["docker", "cp", str(d), f"{container.id}:/home/jovyan/pyproject.toml"])
+    subprocess.run(["docker", "cp", str(d), f"{container.id}:/home/testuser/pyproject.toml"])
     print(container.run("ls -la").output)
     assert container.run("poetry install").exit_code == 0
 
