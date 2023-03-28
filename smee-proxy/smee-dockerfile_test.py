79163e6e632f6e6f39497fb4dee88498907e254b
Jakob J. Kolb <kolbj@rki.de>
2023-03-28 11:04:48 +0200

    Smee: add curl.

--- smee-proxy/smee-dockerfile_test.py
+++ smee-proxy/smee-dockerfile_test.py
@@ -9,3 +9,13 @@ def test_smee(container):
 @pytest.mark.parametrize("container", ["smee-proxy"], indirect=["container"])
 def test_smee_contains_bash(container):
     assert container.exec_run("bash --version").exit_code == 0
+
+
+@pytest.mark.parametrize("container", ["smee-proxy"], indirect=["container"])
+def test_smee_contains_curl(container):
+    assert container.exec_run("curl --version").exit_code == 0
+
+
+@pytest.mark.parametrize("container", ["smee-proxy"], indirect=["container"])
+def test_smee_contains_node(container):
+    assert container.exec_run("node --version").exit_code == 0
