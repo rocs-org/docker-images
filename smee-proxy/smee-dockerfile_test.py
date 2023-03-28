efae9e34a8ebec8078fde5ce4799a45a3d0356ba
Jakob J. Kolb <kolbj@rki.de>
2023-03-28 10:41:26 +0200

    Smee: Add bash to alpine image.

--- smee-proxy/smee-dockerfile_test.py
+++ smee-proxy/smee-dockerfile_test.py
@@ -4,3 +4,8 @@ import pytest
 @pytest.mark.parametrize("container", ["smee-proxy"], indirect=["container"])
 def test_smee(container):
     assert container.status == "created"
+
+
+@pytest.mark.parametrize("container", ["smee-proxy"], indirect=["container"])
+def test_smee_contains_bash(container):
+    assert container.exec_run("bash --version").exit_code == 0
