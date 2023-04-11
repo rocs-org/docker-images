742bbe1205ec621450ca579467bf4b061b68b5a5
Jakob J. Kolb <jakob.j.kolb@gmail.com>
2023-04-11 16:00:16 +0200

    Add jenkins agent with custom certs.

---
+++ jenkins-inbound-agent/jenkins-inbound-agent_test.py
@@ -0,0 +1,13 @@
+import pytest
+import subprocess
+
+
+@pytest.mark.parametrize("container", ["jenkins-inbound-agent"], indirect=["container"])
+def test_certificates_are_present_in_container(container):
+    assert container.status == "created"
+    # print all CA authorities in ca-certificates:
+    authorities = container.run("cat /tmp/ca.txt").output.decode("utf-8")
+    print(authorities)
+    assert "CN = Root-CA" in authorities
+    assert "DC = local, DC = rki, CN = SUB04-CA" in authorities
+
